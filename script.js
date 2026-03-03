c766dd1e6f97a1a943e75298295e8753bd38a1b7
rayhanhossain-69 <bhuiyanrayhanhossain@gmail.com>
2026-03-03 23:31:57 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const p = 1800
+const q = 200
+const division = p/q
+const multiplication = p*q
+const addition = p+q
+const subtraction = p-q
+
+console.log(division)
+console.log(multiplication)
+console.log(addition)
+console.log(subtraction)
