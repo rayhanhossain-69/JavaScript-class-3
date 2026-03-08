054a153013b2ed1c878eee5bc5a92a39753cb577
rayhanhossain-69 <bhuiyanrayhanhossain@gmail.com>
2026-03-08 23:10:26 +0600

    first commit

---
+++ condition.js
@@ -0,0 +1,21 @@
+//Assaignment 1: Pass or Fail Checker//
+
+let marks = 31;
+
+if (marks >= 33) {
+    console.log("Congratulation. You're Pass");
+} else {
+    console.log("Sorry. You're Fail");
+}
+
+
+//Assaignment 2: Positive, Negative Or Zero Checker//
+
+const number = -15
+if(number > 0) {
+    console.log("Positive Number")
+}else if(number < 0) {
+console.log("Negative Number")
+} else {
+    console.log("Zero")
+}
